0f5349237b7d7adbb51a29397abf03f570a20184
Ester Morais <97535906+stehmorais@users.noreply.github.com>
2023-04-04 15:44:22 -0300

    Add files via upload

---
+++ projeto/feriados/core/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+class Feriado(TestCase):
+    def test_natal(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'É Natal')
+
+    def test_tiradentes(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'É Tiradentes')
+
+    def test_nao_feriado(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Não é nenhum dos feriados')
